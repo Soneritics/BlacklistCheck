7b0fdad1558ca338eb29c52bd095c33c2edefe76
Jordi Jolink <info@soneritics.nl>
2017-02-08 15:48:51 +0100

    Add service interface and data contracts

---
+++ BlacklistCheck/BlacklistCheckService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using ServerMonitorShared;
+
+namespace BlacklistCheck
+{
+    public class BlacklistCheckService : IBlacklistCheckService
+    {
+        public List<BlacklistedServer> getBlacklistedIPs(List<string> ips)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BlacklistedServer> getBlacklistedServers(List<Server> servers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool isBlacklisted(Server server)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool isBlacklisted(string ip)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BlacklistCheck/IBlacklistCheckService.cs
@@ -0,0 +1,30 @@
+﻿using ServerMonitorShared;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace BlacklistCheck
+{
+    [ServiceContract]
+    public interface IBlacklistCheckService
+    {
+        [OperationContract]
+        bool isBlacklisted(string ip);
+
+        [OperationContract]
+        bool isBlacklisted(Server server);
+
+        List<BlacklistedServer> getBlacklistedServers(List<Server> servers);
+
+        List<BlacklistedServer> getBlacklistedIPs(List<String> ips);
+    }
+
+    [DataContract]
+    public class BlacklistedServer
+    {
+        public Server server { get; private set; }
+        public List<String> servicesChecked { get; private set; }  = new List<String>();
+        public List<String> blacklisted { get; private set; } = new List<String>();
+    }
+}
